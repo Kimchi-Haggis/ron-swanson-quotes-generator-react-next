68db6ae1a433c54ca6d94bd8a1dc3dc7a87dcff1
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-08-18 22:05:25 +0100

    [Design] brutalist style (#7)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from 'next'
 import { useState } from 'react';
 
@@ -17,15 +19,30 @@ const Home: NextPage = () => {
     }
   }
   return (
-    <div>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-        onClick={getQuotes}
-      >
-        Button
-      </button>
-      <div>
-        {data}
+    <div className='box-section bg-white dark:bg-black py-10'>
+      <div className='box-contents'>
+        <div className='box-item'>
+          <div className='box-item-head border-b-2'>
+            I'M RON FUCKING SWANSON
+          </div>
+          <div className='box-border text-center font-extrabold'>
+            <img 
+              className='mx-auto my-0'
+              src='https://i.pinimg.com/originals/d1/bf/19/d1bf19051c063d9c1cd9361203eb7450.jpg'
+            />
+            <button
+              className="bg-black hover:bg-white border-2 text-white hover:text-black font-bold py-2 px-4"
+              onClick={getQuotes}
+            >
+              GIVE ME ANOTHER
+            </button>
+            <div className='box-item-head'>
+              {data}
+            </div>
+          </div>
+
+
+        </div>
       </div>
     </div>
   )
