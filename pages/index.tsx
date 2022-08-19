1aeda2e14ef11a047eafd57634458ef2fd9729da
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-08-19 14:38:04 +0100

    [Fix] fix invalid characters (#11)
    from ' to &apos;

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       <div className='box-contents'>
         <div className='box-item'>
           <div className='box-item-head border-b-2 border-black dark:border-white'>
-            I'M RON FUCKING SWANSON
+            I&apos;M RON FUCKING SWANSON
           </div>
           <div className='box-border text-center font-extrabold'>
             <img 
